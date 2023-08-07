b2c20afeb18a09558cddd775fbb7b110c95acfd6
Oksana Karpyn <oksana101992@gmail.com>
2023-08-07 18:06:20 +0200

    add modale statico

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 let loadingHidden = document.querySelector('.loading-hidden');
 let singleCard = document.querySelectorAll(".single-card");
 let nome = document.querySelector('#nome');
-
+let modalHidden = document.querySelector('.modal-hidden');
 //let cards = document.querySelectorAll('.cards');
 
   
@@ -15,8 +15,16 @@ singleCard.forEach(function(elem) {
 
     elem.addEventListener("click",function(){
         console.log(elem.dataset.card);
-        loadingHidden.classList.remove('loading-hidden');
+       loadingHidden.classList.remove('loading-hidden');
         nome.innerHTML =`${elem.dataset.card}`;
+        setTimeout(setModal, 3000);
+
 
     })
 });
+
+function setModal(){
+   modalHidden.classList.remove('modal-hidden');
+   loadingHidden.classList.add('loading-hidden');
+
+}
